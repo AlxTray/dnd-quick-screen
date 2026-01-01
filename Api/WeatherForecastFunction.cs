2ba32f1af9626b2dd07e225c594e42531db1b543
AlxTray <alex.ashby02@icloud.com>
2026-01-01 23:23:43 +0000

    update weather template to instead show json of hardcoded character data

--- Api/WeatherForecastFunction.cs
+++ Api/WeatherForecastFunction.cs
@@ -9,49 +9,30 @@ namespace Api
     public class HttpTrigger
     {
         private readonly ILogger _logger;
+        private string characterId;
 
         public HttpTrigger(ILoggerFactory loggerFactory)
         {
             _logger = loggerFactory.CreateLogger<HttpTrigger>();
         }
-
-        [Function("WeatherForecast")]
-        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequestData req)
+        
+        private static HttpClient beyondClient = new()
         {
-            var randomNumber = new Random();
-            var temp = 0;
+            BaseAddress = new Uri("https://character-service.dndbeyond.com"),
+        };
 
-            var result = Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = temp = randomNumber.Next(-20, 55),
-                Summary = GetSummary(temp)
-            }).ToArray();
+        [Function("CharacterData")]
+        public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequestData req)
+        {
+            characterId = "155705536";
+            using HttpResponseMessage characterData = await beyondClient.GetAsync($"character/v5/character/{characterId}");
+            
+            var jsonCharacterData = await characterData.Content.ReadAsStringAsync();
 
             var response = req.CreateResponse(HttpStatusCode.OK);
-            response.WriteAsJsonAsync(result);
+            await response.WriteAsJsonAsync(jsonCharacterData);
 
             return response;
-        }
-
-        private string GetSummary(int temp)
-        {
-            var summary = "Mild";
-
-            if (temp >= 32)
-            {
-                summary = "Hot";
-            }
-            else if (temp <= 16 && temp > 0)
-            {
-                summary = "Cold";
-            }
-            else if (temp <= 0)
-            {
-                summary = "Freezing";
-            }
-
-            return summary;
         }
     }
 }
